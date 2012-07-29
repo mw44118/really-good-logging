2537d1fc138c7466b12eb52eec668e5b703e6da2
W. Matthew Wilson <matt@tplus1.com>
2012-07-29 11:48:39 -0400

    Added tweaks

---
+++ make-finished-stuff.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+cat topics/basic-logging.txt \
+    topics/basic-handlers.txt \
+    topics/beyond-basic-logging.txt \
+    topics/configure-logging-with-config-file.txt \
+    topics/configure-logging-with-dictionary.txt \
+    topics/log-uncaught-exceptions-with-sys.excepthook.txt \
+    topics/multiple-logging-channels.txt \
+    topics/example-log-filters.txt \
+    topics/how-to-log-exceptions.txt \
+    topics/rotate-your-log-files.txt \
+    topics/fun-with-syslog.txt \
+    "topics/shipping-in-process-vs-background.txt" \
+    topics/privacy-and-security-issues.txt \
+    > finished-stuff.txt
