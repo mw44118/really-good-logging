f347c73f4b5e2c5a6b8e502934e399cc5d84783e
W. Matthew Wilson <matt@tplus1.com>
2012-08-01 20:44:29 -0400

    Starting work on sentry

---
+++ rgl/sentryfun.py
@@ -0,0 +1,11 @@
+# vim: set expandtab ts=4 sw=4 filetype=python:
+
+import logging
+
+log = logging.getLogger('rgl')
+
+sentry_credentials = "https://a089e9cd64c6407689d8fb90c9894c64:996762d65bc342ff9c6215aa31b9d9d1@app.getsentry.com/1618"
+
+if __name__ == '__main__':
+
+
